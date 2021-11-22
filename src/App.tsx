3588f267a3d51aaf1400e084ccde4f06ef888d11
tmkn <tuomasklein@gmail.com>
2021-11-22 21:36:44 +0100

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-<div className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
-  Hello World
-</div>
-</>
-  )
+    return (
+        <>
+            <div className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
+                Hello World
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
